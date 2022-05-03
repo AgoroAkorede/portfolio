b2173d8fac05f8250034a096c851170a58893ef6
AgoroAkorede <agoroakorede@gmail.com>
2022-05-03 01:48:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const moreSkills=document.querySelector("#Button")
+
+const positionMarkerLine1=document.getElementById('position_marker-line1');
+const positionMarkerLine2=document.getElementById('position_marker-line2')
+const positionMarkerLine3=document.getElementById('position_marker-line3')
+const positionMarkerLine4=document.getElementById('position_marker-line4')
+const preloader=document.getElementById('preloader')
+const mainContent=document.getElementById('main_content')
+
+function postionMarker1(){
+    setTimeout(() => {
+        positionMarkerLine1.classList.remove('active')
+    }, 3000);
+    positionMarkerLine1.classList.add('active')
+
+    window.scrollTo(0,0)
+}
+function postionMarker2(){
+    setTimeout(() => {
+        positionMarkerLine2.classList.remove('active')
+    }, 3000);
+    positionMarkerLine2.classList.add('active')
+   
+    window.scrollTo(0,600)
+}
+function postionMarker3(){
+    setTimeout(() => {
+        positionMarkerLine3.classList.remove('active')
+    }, 3000);
+    positionMarkerLine3.classList.add('active')
+    window.scrollTo(0,1200)
+}
+function postionMarker4(){
+    setTimeout(() => {
+        positionMarkerLine4.classList.remove('active')
+    }, 3000);
+    positionMarkerLine4.classList.add('active')
+    
+    window.scrollTo(0,1900)
+}
+
+
+
+function work () {
+    const framework=document.getElementById('framework')
+    if(framework.style.display==="flex"){
+        framework.style.display='none' 
+    }
+    else{
+        framework.style.display='flex' 
+    }
+
+}
+
+moreSkills.addEventListener("click", work)
+positionMarkerLine1.addEventListener("click", postionMarker1)
+positionMarkerLine2.addEventListener("click", postionMarker2)
+positionMarkerLine3.addEventListener("click", postionMarker3)
+positionMarkerLine4.addEventListener("click", postionMarker4)
+
+
+window.addEventListener("load",function(){
+    preloader.style.display="none"
+    // mainContent.style.display='flex'
+})
+
+
