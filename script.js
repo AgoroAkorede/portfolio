937b44fdc55090f3e363795150dbb065b659ce58
AgoroAkorede <agoroakorede@gmail.com>
2023-02-01 19:20:55 +0100

    done

--- script.js
+++ script.js
@@ -118,35 +118,18 @@ toggleModeMobile.addEventListener("click", changeBackground);
 
 // -----------loading-----------
 
-// const before_loadtime = new Date().getTime();
-
-// function animateValue(obj, start, end, duration) {
-
-//   let startTimestamp = null;
-//   const step = (timestamp) => {
-//     if (!startTimestamp) startTimestamp = timestamp;
-//     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-//     obj.innerHTML = Math.floor(progress * (end - start) + start);
-//     if (progress < 1) {
-//       window.requestAnimationFrame(step);
-//     }
-//   };
-//   window.requestAnimationFrame(step);
-
-// }
-
-// animateValue(obj, 0, 100, 3000);
-
 function animateValue(obj, start, end, duration) {
   let startTimestamp = null;
   const step = (timestamp) => {
     if (!startTimestamp) startTimestamp = timestamp;
-    function animateValue(obj, start, end, duration) {}
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
   };
   window.requestAnimationFrame(step);
 }
 
 const obj = document.getElementById("value");
 animateValue(obj, 0, 100, 3000);
-
-
